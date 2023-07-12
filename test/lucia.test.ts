69f447af927e47facdaabd3f42760bb426a7f1fc
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2023-07-12 20:38:54 +0200

    feat: added tests

---
+++ test/lucia.test.ts
@@ -0,0 +1,71 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { eq } from "drizzle-orm";
+import { auth } from "../src/lucia";
+import { dbPool } from "../src/db";
+import { user as UserSchema } from "../src/schema"; // Assuming User type is exported from here
+
+const TEST_USER = {
+  id: "1337",
+  password: "password",
+};
+
+beforeAll(async () => {
+  await cleanup();
+});
+
+afterAll(async () => {
+  await cleanup();
+});
+
+describe("Authentication", () => {
+  it("should register a user", async () => {
+    const user = await auth.createUser({
+      key: {
+        providerId: "test",
+        providerUserId: TEST_USER.id,
+        password: TEST_USER.password,
+      },
+      attributes: {},
+      userId: TEST_USER.id,
+    });
+    expect(user.userId).toBe(TEST_USER.id);
+  });
+
+  it("should not login and throw invalid password", async () => {
+    try {
+      await auth.useKey("test", TEST_USER.id, "wr0ngp4ssw0rd");
+      throw new Error("Expected useKey to throw, but it did not");
+    } catch (error) {
+      if (error instanceof Error) {
+        expect(error.message).toBe("AUTH_INVALID_PASSWORD");
+      } else {
+        throw new Error("Expected an Error to be thrown, but it was not");
+      }
+    }
+  });
+
+  it("should create session", async () => {
+    try {
+      const user = await auth.useKey("test", TEST_USER.id, TEST_USER.password);
+      const session = await auth.createSession({
+        userId: user.userId,
+        attributes: {},
+      });
+      expect(session.user.userId).toBe(TEST_USER.id);
+    } catch (error) {
+      throw new Error("Expected useKey to not throw, but it did: " + error);
+    }
+  });
+});
+
+const cleanup = async () => {
+  const existingUser = await dbPool
+    .select({ userId: UserSchema.id })
+    .from(UserSchema)
+    .where(eq(UserSchema.id, TEST_USER.id));
+  const userId = existingUser[0]?.userId;
+
+  await auth.invalidateAllUserSessions(userId);
+  await auth.deleteDeadUserSessions(userId);
+  await auth.deleteUser(userId);
+};
