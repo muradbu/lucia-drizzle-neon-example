156ce11e985e3f6218a4ae39cbbeb24aff7f338f
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2023-07-13 13:59:42 +0200

    fix: conditional import for edge environment

--- src/db.ts
+++ src/db.ts
@@ -2,8 +2,11 @@ import { drizzle as drizzleWs } from "drizzle-orm/neon-serverless";
 import { drizzle as drizzleHttp } from "drizzle-orm/neon-http";
 import { Pool, neon, neonConfig } from "@neondatabase/serverless";
 import ws from "ws";
-import * as dotenv from "dotenv";
-dotenv.config();
+
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = await import("dotenv");
+  dotenv.config();
+}
 
 // This is required for the WebSocket connection to work in Node.js
 // See: https://github.com/neondatabase/serverless#example-nodejs-with-poolconnect
